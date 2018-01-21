2ff0dde7e21c007b0887511efced6a137ac32cc1
moinali <35587007+moinali@users.noreply.github.com>
2018-01-21 02:52:01 -0600

    Update homework1.py

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -1,26 +1,19 @@
-def get_hours_since_midnight(seconds):
-    '''
-    Type the code to calculate total hours given n(number) of seconds
-    For example, given 3800 seconds the total hours is 1
-    '''
-    return
+#seconds since midnight to return hours, minutes, and seconds
+#1 min = 60
+#1 hour = 60 * 60 = 3600
 
-'''
-IF YOU ARE OK WITH A GRADE OF 70 FOR THIS ASSIGNMENT STOP HERE.
-'''
+def get_hours_since_midnight(seconds):
+    return (seconds//3600)
 
 def get_minutes(seconds):
-    '''
-    Type the code to calculate total minutes less whole hour given n(number) of seconds
-    For example, given 3800 seconds the total minutes is 3
-    '''
-
-    return
+    return (seconds - (get_hours_since_midnight(seconds)*3600))//60
 
 def get_seconds(seconds):
-    '''
-    Type the code to calculate total minutes less whole hour given n(number) of seconds
-    For example, given 3800 seconds the total minutes is 20
-    '''
+    return seconds - ((get_hours_since_midnight(seconds)*3600) + (get_minutes(seconds)*60))
 
-    return
+def time():
+    seconds= int(input('enter amount of seconds: '))
+    print('Total time eclipse since midnight in hh:mm:ss is ', get_hours_since_midnight(seconds), ':', get_minutes(seconds), ':',get_seconds(seconds) )
+    
+    
+time()
