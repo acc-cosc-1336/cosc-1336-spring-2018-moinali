0833ce6b59aebcd2535d59c2283482f7c968a915
moinali <35587007+moinali@users.noreply.github.com>
2018-02-15 21:40:49 -0600

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -1,4 +1,4 @@
-import src.homework.main.main_homework4
+import src.assignments.main_assignment5
 
 #running this file will run the code in main_homework2.py file without the need to modify
 #code in that file
