0ddcf202e6d09ce54008781cc8faeee672063228
moinali <35587007+moinali@users.noreply.github.com>
2018-02-28 15:58:45 -0600

    Add files via upload

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tests.assignments import test_assignment6
+from tests.homework import test_homework6
 
-suite = unittest.TestLoader().loadTestsFromModule(test_assignment6)
+suite = unittest.TestLoader().loadTestsFromModule(test_homework6)
 unittest.TextTestRunner(verbosity=2).run(suite)
