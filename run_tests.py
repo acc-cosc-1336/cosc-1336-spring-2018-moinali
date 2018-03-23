f8b07f2b6486753295d83f145dd67c0baa438f84
moinali <35587007+moinali@users.noreply.github.com>
2018-03-23 00:34:02 -0500

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.midterm import test_exam
+from tests.homework import test_homework7
 
-suite = unittest.TestLoader().loadTestsFromModule(test_exam)
+suite = unittest.TestLoader().loadTestsFromModule(test_homework7)
 unittest.TextTestRunner(verbosity=2).run(suite)
