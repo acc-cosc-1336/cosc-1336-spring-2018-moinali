fbdfc038789635fb8d9706a0a1a2622abe551465
moinali <35587007+moinali@users.noreply.github.com>
2018-03-29 22:06:49 -0500

    Add files via upload

---
+++ src/assignments/main.py
@@ -0,0 +1,30 @@
+#Write import statements for classes invoice and invoice_item
+from invoice import Invoice
+from invoice_item import InvoiceItem
+'''
+LOOK AT THE TEST CASES FOR HINTS
+Create an invoice object
+In the loop:
+Create a new InvoiceItem
+Create a user controlled loop to continue until y is not typed, in loop...
+    Prompt user for description, quantity, and cost.
+    Add values to the InvoiceItem.
+    Add the InvoiceItem to the invoice object.
+    Once user types a letter other than y display the Invoice to screen
+'''
+
+
+invoice = Invoice('ABC Company', '03282018')
+loop = 'y'
+while loop == 'y':
+    description = input('enter description')
+    quantity = int(input('enter quantity'))
+    cost = float(input('enter cost'))
+    
+    invoice_item = InvoiceItem(description, quantity, cost)
+    invoice.add_invoice_item(invoice_item)
+    
+    loop = input('enter y to cont.')
+
+    if loop != 'y':
+        invoice.print_invoice()
