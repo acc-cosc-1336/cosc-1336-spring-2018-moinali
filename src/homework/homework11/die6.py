0d5deddd251fc46f7239476266275de698dc18b9
moinali <35587007+moinali@users.noreply.github.com>
2018-04-17 18:06:49 -0500

    Add files via upload

---
+++ src/homework/homework11/die6.py
@@ -0,0 +1,20 @@
+from src.homework.homework11.die import Die
+#HOMEWORK12: add import statement for Die class
+'''
+Define a Die6 class that inherits from the Die class.
+Create a constructor with only one parameter self.
+In the constructor, call the Die class constructor(__init__ method) and pass it parameter arguments self and 6.
+Define a class method named roll.
+In the method:
+ a)display 'Rolled 6 sided die' (MAKE SURE TO USE THE SIDES CLASS ATTRIBUTE; DON'T USE THE NUMBER 6).
+ b)call and return the Die class roll method(This will return the roll value).
+
+'''
+class Die6(Die):
+    def __init__(self):
+        Die.__init__(self,6)
+
+    def roll(self):
+        print('Rolled 6 sided die')
+        self.value = random.randint(1, self.sides)
+        return self.value
---
+++ src/homework/homework11/main.py
@@ -0,0 +1,30 @@
+from src.homework.homework11.player import Player
+from src.homework.homework11.game_log import GameLog
+from src.homework.homework11.die6 import Die6
+from src.homework.homework11.die8 import Die8
+#write import statements for Die6 and Die8 classes
+##src.homework.homework11.
+game_log = GameLog()
+d6 = Die6()
+d8 = Die8()
+
+#ASSIGNMENT 12: Write statements to create Die6 and Die8 instances
+
+#ASSIGNMENT12: pass the Die6 and Die8 instance object variables to the Player instantiation below as parameters after
+#the game_log parameter
+Player(game_log,d6,d8).roll_doubles()
+
+game_log.display_log()
+
+#ASSIGNMENT12: Create another GameLog instance
+#ASSIGNMENT12: Write statements to create Die6 and Die8 instances
+#ASSIGNMENT12: Create a new instance of the Player class and pass it the game log, die6, and die8 instances.
+#ASSIGNMENT12: Call the player instance roll_doubles.
+#ASSIGNMENT12: Call the game log instance display_log method.
+
+d12 = Die6()
+d16 = Die8()
+
+Player(game_log, d12, d16).roll_doubles()
+
+game_log.display_log()
